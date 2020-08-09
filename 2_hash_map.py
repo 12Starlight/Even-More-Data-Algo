d5f9a83c697f4208a3a8ec42ac6f0ee60dfceabc
Dave <dave.gagnat@gmail.com>
2020-08-09 15:01:42 -0500

    Added HashMap code snippets with notes

---
+++ 2_hash_map.py
@@ -0,0 +1,40 @@
+# HashMap
+  # lookup, insert, delete O(1) Time
+  # not ordered
+    # key is hashed, moded, and put in bucket
+    # collisions are turned into a LinkedList
+# Type 'python' in terminal for interactive terminal
+
+
+# Create
+hash = {}
+hash['a'] = 1
+hash['b'] = 2
+hash
+
+# Iterate
+for key in hash:
+  print(key)
+
+# Print Key Value Pairs
+for key in hash:
+  print(key, hash[key])
+
+# Print Hash
+print(hash)
+
+# Check If Key Exists
+print('a' in hash) 
+print('x' in hash)
+
+# Lookup Value
+print(hash['a'])
+
+# Delete Item
+del hash['a']
+print(hash)
+
+# Not ordered
+hash['c'] = 3
+hash['a'] = 1
+print(hash)
