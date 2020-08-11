74b323684482944ae21ce9476448dbfd653dddb3
Dave <dave.gagnat@gmail.com>
2020-08-11 11:11:23 -0500

    Added Binary Tree worksheet

---
+++ 6_worksheet_binary_tree.py
@@ -0,0 +1,37 @@
+'''
+Binary Tree:
+  -> A tree is a data structure that has a heirachical structure where there are
+  parents and children, unlike normal graphs.
+  -> A binary tree is a tree in which each node has at most two children, left
+  and right.
+  -> There are different ways of traversing a binary tree in a Depth First 
+  Search mannerr:
+    a.) Preorder: Node, Left, Right
+    b.) Inorder: Left, Node, Right
+    c.) Postorder: left, right, node
+
+  -> You can traverse trees recursively and iteratively.
+  -> You can traverse a tree with BFS (Breadth First Search), useful for 'level
+  order traversal'.
+
+'''
+
+
+# Binary Tree Preorder Traversal
+
+# Binary Tree Inorder Traversal
+
+# Binary Tree Inorder Traversal
+
+# Binary Tree Level Order Traversal
+
+
+'''
+Exercise 2
+
+'''
+
+
+# Maximum Depth of Binary Tree
+
+# Symmetric Tree
