1ee4e339a7bf9adfd48e4f1b10d19c4107aa00e1
Dave <dave.gagnat@gmail.com>
2020-08-11 10:57:57 -0500

    Added notes to Linked List

--- 4_reverse_linked_list.py
+++
@@ -1,7 +0,0 @@
-# Reverse A Singly Linked List  
-
-# Remove Linked List Elements
-
-# Odd Even Linked List
-
-# Palindrome Linked List
---
+++ 4_worksheet_linked_list.py
@@ -0,0 +1,25 @@
+'''
+Linked Lists:
+  -> Learn to reverse a Linked List!
+  -> A Linked List is a chain of Nodes. A Node is composed of a Value and a
+  Pointer to the next Node (usually called next)
+  -> Use a dummy node when you are not sure what the Head of the LinkedList will
+  be.
+  -> Most of the time, you will have to use multiple pointers to solve these
+  questions.
+  -> Useful trick: To detect cycle, have one pointer move twice as fast as the
+  other pointer.
+  -> Doubly Linked Lists are Linked Lists that have two pointers, one pointing
+  to the next node and one pointing to its previous node. 
+
+
+'''
+
+
+# Reverse A Singly Linked List  
+
+# Remove Linked List Elements
+
+# Odd Even Linked List
+
+# Palindrome Linked List
