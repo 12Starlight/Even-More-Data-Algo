4d4c2354f1b0c96cb5db5e94195a1e6a7be46a67
Dave <dave.gagnat@gmail.com>
2020-08-12 13:37:40 -0500

    Added quickSort(arr)

--- 8_sorting.py
+++ 8_sorting.py
@@ -8,7 +8,10 @@ Sorting
 '''
 
 
-arr = [1, 2, 8, 9, 5, 3, 4, 7, 6]
+arr1 = [1, 2, 8, 9, 5, 3, 4, 7, 6]
+arr2 = [1, 2, 8, 9, 5, 3, 4, 7, 6]
+arr3 = [1, 2, 8, 9, 5, 3, 4, 7, 6]
+arr4 = [1, 2, 8, 9, 5, 3, 4, 7, 6]
 
 
 # iterates through array swapping elements to find the smallest
@@ -19,8 +22,8 @@ def bubbleSort(arr):
       if arr[j] > arr[j + 1]:
         arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-bubbleSort(arr)
-print(arr)
+bubbleSort(arr1)
+print(arr1)
 print('')
 
 # inserts every array element into its proper position by shifting the number
@@ -33,8 +36,8 @@ def insertionSort(arr):
       j -= 1
     arr[j + 1] = key
 
-insertionSort(arr)
-print(arr)
+insertionSort(arr2)
+print(arr2)
 print('')
 
 # iterates through array finding smallest element to insert
@@ -46,6 +49,29 @@ def selectionSort(arr):
         min_idx = j
     arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
-selectionSort(arr)
-print(arr)
+selectionSort(arr3)
+print(arr3)
+print('')
+
+# QuickSort
+def quickSort(arr, low, high):
+  if low < high:
+    pi = partition(arr, low, high)
+    quickSort(arr, low, pi - 1)
+    quickSort(arr, pi + 1, high)
+
+def partition(arr, low, high):
+  i = low - 1
+  pivot = arr[high]
+  for j in range(low, high):
+    if arr[j] <= pivot:
+      # increment index of smaller element
+      i = i + 1
+      # swap
+      arr[i] , arr[j] = arr[j], arr[i]
+  arr[i + 1], arr[high] = arr[high], arr[i + 1]
+  return i + 1
+
+quickSort(arr4, 0, 8)
+print(arr4)
 print('')
