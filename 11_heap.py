fad4cc841a7c3ca671a90b0407f3515d80882d09
Dave <dave.gagnat@gmail.com>
2020-08-13 15:24:12 -0500

    Added heapSort(arr)

--- 11_heap.py
+++ 11_heap.py
@@ -78,3 +78,23 @@ def heapify(arr, n, i):
     # Heapify the root
     heapify(arr, n, largest)
 
+# The main function to sort an array of given size
+def heapSort(arr):
+  n = len(arr)
+
+  # Build a maxheap
+  for i in range(n, -1, -1):
+    heapify(arr, n, i)
+
+  # One by one extract elements
+  for i in range(n - 1, 0 -1):
+     arr[i], arr[0] = arr[0], arr[i] # swap
+     heapify(arr, i, 0)
+
+# Driver code to test above
+array = [12, 11, 13, 5, 6, 7]
+heapSort(array)
+n = len(array)
+print('')
+for i in range(n):
+  print("%d" %array[i])
